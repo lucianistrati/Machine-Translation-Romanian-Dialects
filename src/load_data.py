c373da8472a8e768ae9a068fd59d95b73eb9e105
lucianistrati <lucianistrati@yahoo.com>
2022-11-25 19:36:22 +0200

    added books and videos

---
+++ src/load_data.py
@@ -0,0 +1,15 @@
+"""
+here there should be functions on how to read pdfs, txts and then parse them and clean them so that they are good to
+go in the other components
+"""
+import os
+
+books_folderpath = "data/books"
+
+videos_folderpath = "data/videos"
+
+classes = ["crisanean", "maramuresean", "banatean", "ardelean", "oltean", "moldovean", "muntean"]
+
+class_to_book = {}
+
+class_to_video = {}
