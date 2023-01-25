277cb1a76c06d3b40289badb44484241c3529ec9
lucianistrati <lucianistrati@yahoo.com>
2023-01-25 19:01:36 +0200

    loaded data from pdfs and transcripts, also made translation by replacement available

--- src/load_data.py
+++ src/load_data.py
@@ -32,12 +32,83 @@ class_to_video = {'crisanean': [''],
                   'muntean': [''],
                   'timocean': ['About our village and our language – Timok Romanian (Vlach) Collection.mp4']}
 
-def load_data():
-    pass
+
+video_to_alias = {'Curs de dialect ardelenesc!.mp4': 'ardelenesc.txt',
+                  'Curs de dialect ardelenesc!suduieli si blesteme!.mp4': 'ardelenesc_suduieli.txt',
+                  'Stirile in limba moldoveneasca.mp4': 'stiri_moldoveneasca.txt',
+                  'Igor Dodon explică strategia prin care doreşte să mute graniţa dintre România şi Republica Moldova.mp4': 'dodon.txt',
+                  'Vox pop Unirea cu România şi preţul ei.mp4': 'vox_pop_pretul_unirii.txt',
+                  'Vox populi Tinerii despre România şi Unire.mp4': 'vox_pop_romania_unire.txt',
+                  'About our village and our language – Timok Romanian (Vlach) Collection.mp4': 'timok.txt'}
+
+def load_data(filepath: str):
+    to_str = False
+    if filepath.endswith('.txt'):
+        texts = []
+        if "vatis" in filepath:
+            with open(filepath, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    if len(line) > 0 and line.startswith("[") is False:
+                        texts.append(line)
+        elif "sonix" in filepath:
+            with open(filepath, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line[line.find(" "):]
+                    tokens = [token for token in line.split()
+                              if token.startswith("[") is False
+                              and token.endswith("]") is False]
+                    texts.append(" ".join(tokens))
+        if to_str:
+            return " ".join(texts)
+        return texts
+    elif filepath.endswith('.pdf'):
+        # importing required modules
+        from PyPDF2 import PdfReader
+        # creating a pdf reader object
+        reader = PdfReader(filepath)
+        # printing number of pages in pdf file
+        whole_text = []
+        for page in reader.pages:
+            # extracting text from page
+            text = page.extract_text()
+            whole_text.append(text)
+        if to_str:
+            return " ".join(whole_text)
+    else:
+        raise Exception("Unsupported format!")
 
 
 def main():
-    pass
+    books_contents = []
+    for file in os.listdir("data/books"):
+        filepath = os.path.join("data/books", file)
+        texts = load_data(filepath)
+        books_contents.append(texts)
+    print(len(books_contents))
+    with open("data/books/all_books_contents.txt", "w") as f:
+        f.write(str(books_contents))
+
+    sonix_transcriptions = []
+    for file in os.listdir("data/transcriptions/sonix"):
+        filepath = os.path.join("data/books", file)
+        texts = load_data(filepath)
+        sonix_transcriptions.append(texts)
+    print(len(sonix_transcriptions))
+
+    with open("data/transcriptions/sonix_transcriptions.txt", "w") as f:
+        f.write(str(sonix_transcriptions))
+
+    vatis_transcriptions = []
+    for file in os.listdir("data/transcriptions/vatis_tech"):
+        filepath = os.path.join("data/books", file)
+        texts = load_data(filepath)
+        vatis_transcriptions.append(texts)
+    print(len(vatis_transcriptions))
+
+    with open("data/transcriptions/vatis_transcriptions.txt", "w") as f:
+        f.write(str(vatis_transcriptions))
 
 
 if __name__ == "__main__":
--- src/translation.py
+++ src/translation.py
@@ -1,24 +1,99 @@
 """
 translation inference
 """
-def translate_from_dialect_to_formal(text, initial_dialect):
+from src.utils import dialectical_rules
+import random
+
+def text_to_speech(text):
+    speech = None
+    return speech
+
+
+def translate_from_dialect_to_formal(text, initial_dialect, modality: str="text"):
     """
 
     :param text:
     :param initial_dialect:
     :return:
     """
-    pass
+    fonetical_rules = dialectical_rules[initial_dialect]["fonetical_rules"]
+    gramatical_rules = dialectical_rules[initial_dialect]["gramatical_rules"]
 
+    if "fonetical_examples" in dialectical_rules[initial_dialect]:
+        fonetical_examples = dialectical_rules[initial_dialect]["fonetical_examples"]
+        for (k, v) in fonetical_examples.items():
+            fonetical_rules[k] = v
 
-def translate_from_formal_to_dialect(text, final_dialect):
+    if "lexical_examples" in dialectical_rules[initial_dialect]:
+        lexical_examples = dialectical_rules[initial_dialect]["lexical_examples"]
+        for (k, v) in lexical_examples.items():
+            gramatical_rules[k] = v
+
+    if "gramatical_examples" in dialectical_rules[initial_dialect]:
+        gramatical_examples = dialectical_rules[initial_dialect]["gramatical_examples"]
+        for (k, v) in gramatical_examples.items():
+            gramatical_rules[k] = v
+
+    if modality == "text":
+        rules = gramatical_rules
+    elif modality == "speech":
+        rules = fonetical_rules
+    else:
+        raise Exception("Wrong!")
+
+    for (literal_form, dialectical_form) in rules.keys():
+        if isinstance(dialectical_form, list):
+            dialectical_form = random.choice(dialectical_form)
+        text = text.replace(dialectical_form, literal_form)
+
+    if modality == "speech":
+        speech = text_to_speech(text)
+        return speech
+
+    return text
+
+def translate_from_formal_to_dialect(text, final_dialect, modality: str="text"):
     """
 
     :param text:
     :param final_dialect:
     :return:
     """
-    pass
+    fonetical_rules = dialectical_rules[final_dialect]["fonetical_rules"]
+    gramatical_rules = dialectical_rules[final_dialect]["gramatical_rules"]
+
+    if "fonetical_examples" in dialectical_rules[final_dialect]:
+        fonetical_examples = dialectical_rules[final_dialect]["fonetical_examples"]
+        for (k, v) in fonetical_examples.items():
+            fonetical_rules[k] = v
+
+    if "lexical_examples" in dialectical_rules[final_dialect]:
+        lexical_examples = dialectical_rules[final_dialect]["lexical_examples"]
+        for (k, v) in lexical_examples.items():
+            gramatical_rules[k] = v
+
+    if "gramatical_examples" in dialectical_rules[final_dialect]:
+        gramatical_examples = dialectical_rules[final_dialect]["gramatical_examples"]
+        for (k, v) in gramatical_examples.items():
+            gramatical_rules[k] = v
+
+    if modality == "text":
+        rules = gramatical_rules
+    elif modality == "speech":
+        rules = fonetical_rules
+    else:
+        raise Exception("Wrong!")
+
+    for (literal_form, dialectical_form) in rules.keys():
+        if isinstance(dialectical_form, list):
+            dialectical_form = random.choice(dialectical_form)
+        text = text.replace(literal_form, dialectical_form)
+
+    if modality == "speech":
+        speech = text_to_speech(text)
+        return speech
+
+    return text
 
 
 def translate(text: str, initial_dialect: str, final_dialect: str):
@@ -29,12 +104,25 @@ def translate(text: str, initial_dialect: str, final_dialect: str):
     :param final_dialect:
     :return:
     """
-    formal_text = translate_from_dialect_to_formal(text, initial_dialect)
-    final_translated_text = translate_from_formal_to_dialect(text, final_dialect)
+    if initial_dialect == final_dialect:
+        return "You need to translate between different dialects!"
+
+    if initial_dialect != "formal":
+        formal_text = translate_from_dialect_to_formal(text, initial_dialect)
+    else:
+        formal_text = text
+
+    if final_dialect != "formal":
+        final_translated_text = translate_from_formal_to_dialect(formal_text, final_dialect)
+    else:
+        final_translated_text = formal_text
+
     return final_translated_text
 
+
 def main():
-    pass
+    text = "Ana are mere"
+    translate(text, "muntean", "moldovean")
 
 
 if __name__ == "__main__":
